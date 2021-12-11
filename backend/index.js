9bb7689a6145c2df8548ba74cbe101ddfed39382
Yong Qin <yongqin3759@gmail.com>
2021-12-12 02:44:53 +0800

    changed port commit

--- backend/index.js
+++ backend/index.js
@@ -119,7 +119,7 @@ app.post('/api/persons', (req,res)=>{
     res.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
